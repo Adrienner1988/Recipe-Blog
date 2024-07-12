67341e91fdbb31b31928f16d046b9cc541771fba
adrienner1988 <adriennerdaniels@gmail.com>
2024-07-12 18:17:39 -0400

    Made folders for components and the pages that will be viewable. Installed reaact router dom and set up routing to  each component.

---
+++ src/views/AddRecipePage.tsx
@@ -0,0 +1,10 @@
+const AddRecipePage = () => {
+
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default AddRecipePage
---
+++ src/views/RecipePage.tsx
@@ -0,0 +1,10 @@
+const RecipePage = () => {
+
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default RecipePage
