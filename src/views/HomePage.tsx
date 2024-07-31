6d21e98963de6da20ed4c27f5d2bc2c4b1720fd7
adrienner1988 <adriennerdaniels@gmail.com>
2024-07-30 22:15:36 -0400

    Installed TailwindCSS

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,8 +1,8 @@
 const HomePage = () => {
   return (
     <>
-      <h1>Welcome The Recipe Blog</h1>
-      <p>
+      <h1 className="text-3xl font-bold underline">Welcome The Recipe Blog</h1>
+      <p className="">
         Explore and enjoy a variety of recipes from traditional and modern
         culinary worlds.
       </p>
