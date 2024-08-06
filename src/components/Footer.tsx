cb060e35fede413919fdf977becec16bde50b8e6
adrienner1988 <adriennerdaniels@gmail.com>
2024-08-06 14:02:43 -0400

    Setup the footer with the color scheme and styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,16 @@
 const Footer = () => {
-  return 
-  <>
-  
-  </>;
+  return (
+    <>
+      <footer className="bg-darkPlum text-green text-lg p-2">
+        <div className="flex justify-center space-x-6">
+          <p className="text-center mb-1">
+            {new Date().getFullYear()} Recipe Rainbow. All rights reserved.
+          </p>
+          <p className="text-center">Get in touch</p>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
