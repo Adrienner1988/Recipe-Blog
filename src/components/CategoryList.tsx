b3b0010049d60f87d73ef278f5544c4af0fb5b1b
adrienner1988 <adriennerdaniels@gmail.com>
2024-08-14 01:48:20 -0400

    Added in sytling on the category list page. Rounded the images, hover styling.

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -12,18 +12,28 @@ interface CategoriesListProps {
 
 const CategoriesList: React.FC<CategoriesListProps> = ({ categories }) => {
   return (
-    <div className="categories-container">
-      {categories.map((category: any) => (
-        <Link
-          key={category.id}
-          to={`/recipes?category=${category.id}`}
-          className="category-card"
-        >
-          <img src={category.image} alt={category.name} />
-          <h3>{category.name}</h3>
-        </Link>
-      ))}
-    </div>
+    <>
+      <div className="flex overflow-x-auto whitespace-nowrap py-4">
+        {categories.map((category) => (
+          <Link
+            key={category.id}
+            to={`/recipes?category=${category.id}`}
+            className="relative inline-block w-48 h-48 mx-2 rounded-full overflow-hidden shadow-custom-light transition-transform duration-300 hover:scale-105 group"
+          >
+            <img
+              src={category.image}
+              alt={category.name}
+              className="w-full h-full object-cover transition-opacity duration-300 group-hover:opacity-40"
+            />
+            <div className="absolute inset-0 flex items-center justify-center bg-opacity-40 rounded-full opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <h3 className="text-gray text-lg font-semibold group-hover:text-lightPlum transition-colors duration-300 uppercase">
+                {category.name}
+              </h3>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -34,6 +34,7 @@ export default {
       boxShadow: {
         "custom-light": "0px 4px 8px rgba(0, 0, 0, 0.1)",
         "custom-dark": "0px 6px 12px rgba(0, 0, 0, 0.2)",
+        "text-shadow": "1px 1px 2px rgba(0, 0, 0, 0.8)",
       },
     },
   },
