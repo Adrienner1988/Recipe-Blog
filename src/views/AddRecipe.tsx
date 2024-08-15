308434f16ca44c2e04304b09241d2bfd58dc2b1f
adrienner1988 <adriennerdaniels@gmail.com>
2024-08-14 22:48:53 -0400

    Updated the index.css with stylings to the textarea and button to clean up the AddRecipe page and added in the additional inputs to the  recipe form.

--- src/views/AddRecipe.tsx
+++ src/views/AddRecipe.tsx
@@ -6,6 +6,10 @@ const AddRecipe = () => {
   const [ingredients, setIngredients] = useState("");
   const [steps, setSteps] = useState("");
   const [image, setImage] = useState<File | null>(null);
+  const [prep, setPrep] = useState("");
+  const [cook, setCook] = useState("");
+  const [servings, setServings] = useState("");
+  
   const navigate = useNavigate();
 
   const handleSubmit = async (event: React.FormEvent) => {
@@ -15,9 +19,9 @@ const AddRecipe = () => {
     formData.append("title", title);
     formData.append("ingredients", ingredients);
     formData.append("steps", steps);
-    // formData.append("prep", prep);
-    // formData.append("cook", cook);
-    // formData.append("servings", servings);
+    formData.append("prep", prep);
+    formData.append("cook", cook);
+    formData.append("servings", servings);
     if (image) formData.append("image", image);
 
     try {
@@ -36,54 +40,70 @@ const AddRecipe = () => {
   return (
     <>
       <div>
-        <h2>Add Recipe</h2>
+        <h2>Add Your Recipe</h2>
       </div>
 
       <div className="flex justify-center">
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={title}
-            onChange={(event) => setTitle(event.target.value)}
-            placeholder="Title"
-            required
-            className="border-2 border-solid border-darkPlum transition-all duration-500 hover:border-green rounded-xl w-full p-2 mb-2"
-          />
-          {/* <input
-            type="text"
-            value={prep}
-            onChange={(event) => setTitle(event.target.value)}
-            placeholder="Title"
-            required
-            className="border-2 border-solid border-darkPlum transition-all duration-500 hover:border-green rounded-xl w-full p-2 mb-2"
-          /> */}
-          <textarea
-            value={ingredients}
-            onChange={(e) => setIngredients(e.target.value)}
-            placeholder="Ingredients"
-            required
-            className="border-2 border-solid border-darkPlum transition-all duration-500 hover:border-green rounded-xl w-full p-2 mb-2"
-          />
-          <textarea
-            value={steps}
-            onChange={(e) => setSteps(e.target.value)}
-            placeholder="Steps"
-            required
-            className="border-2 border-solid border-darkPlum transition-all duration-500 hover:border-green rounded-xl w-full p-2 mb-2"
-          />
-          <input
-            type="file"
-            onChange={(e) => setImage(e.target.files?.[0] || null)}
-            required
-            className="border-2 border-solid border-darkPlum transition-all duration-500 hover:border-green rounded-xl w-full p-2 mb-2"
-          />
-          <button
-            type="submit"
-            className="border-none bg-darkPlum rounded-xl p-2 uppercase text-green transition-all duration-500 hover:text-grayLight cursor-pointer"
+        <div className="w-full max-w-lg">
+          <form
+            onSubmit={handleSubmit}
+            className="bg-green p-6 rounded-lg shadow-custom-dark"
           >
-            Submit
-          </button>
-        </form>
+            <input
+              type="text"
+              value={title}
+              onChange={(event) => setTitle(event.target.value)}
+              placeholder="Title"
+              required
+            />
+            <input
+              type="text"
+              value={prep}
+              onChange={(event) => setPrep(event.target.value)}
+              placeholder="Title"
+              required
+            />
+            <input
+              type="text"
+              value={cook}
+              onChange={(event) => setCook(event.target.value)}
+              placeholder="Cook Time"
+              required
+            />
+            <input
+              type="text"
+              value={servings}
+              onChange={(event) => setServings(event.target.value)}
+              placeholder="Servings"
+              required
+            />
+            <textarea
+              value={ingredients}
+              onChange={(e) => setIngredients(e.target.value)}
+              placeholder="Ingredients, add each new ingredient to a new line."
+              required
+            />
+            <textarea
+              value={steps}
+              onChange={(e) => setSteps(e.target.value)}
+              placeholder="Steps, add one empty line between each new step."
+              required
+    
+            />
+            <input
+              type="file"
+              onChange={(e) => setImage(e.target.files?.[0] || null)}
+              required
+              className="border-2 border-solid border-darkPlum transition-all duration-500 hover:border-lightPlum rounded-xl w-full p-2 mb-2"
+            />
+            <button
+              type="submit"
+              className="border-none bg-darkPlum rounded-xl p-2 uppercase text-green transition-all duration-500 hover:text-grayLight cursor-pointer"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
